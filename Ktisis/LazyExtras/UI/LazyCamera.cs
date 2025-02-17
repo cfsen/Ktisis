a1a336f5faea714b10f73c1fcda08f3f70d1e07a
cfsen <103598332+cfsen@users.noreply.github.com>
2025-02-17 13:06:18 +0100

    LazyCamera: FOV slider

--- Ktisis/LazyExtras/UI/LazyCamera.cs
+++ Ktisis/LazyExtras/UI/LazyCamera.cs
@@ -14,6 +14,7 @@ using Ktisis.Interface.Editor.Types;
 using System.Runtime.CompilerServices;
 using System.Linq;
 using Ktisis.LazyExtras.Components;
+using Ktisis.Editor.Camera.Types;
 
 namespace Ktisis.Interface.Windows;
 
@@ -21,6 +22,9 @@ public class LazyCamera :KtisisWindow {
 	private readonly IEditorContext _ctx;
 	private readonly GuiManager _gui;
 	private readonly LazyCameraComponents _lazyCameraComponents;
+
+	private bool _DelimitMinFoV = false;
+
 	public LazyCamera(
 		IEditorContext ctx,
 		GuiManager gui
@@ -44,7 +48,14 @@ public class LazyCamera :KtisisWindow {
 	}
 
 	public override void Draw() {
+		// TODO Disable all interactions when work camera is enabled
+		this.SetCamFov();
+		this.GetCamData();
 		this.CameraList();
+	}
+
+	private void NavControls() {
+
 	}
 
 	private void CameraList() {
@@ -57,6 +68,11 @@ public class LazyCamera :KtisisWindow {
 			ImGui.SameLine();
 			var xoffset = ImGui.GetWindowSize().X - 100.0f;
 			ImGui.SetCursorPosX(xoffset);
+
+			// Disable button for active camera 
+			if(cam == this._ctx.Cameras.Current)
+				ImGui.BeginDisabled();
+
 			if(ImGui.Button(
 				$"Delete##lazyCamDelBtn{i}", 
 				new Vector2 {
@@ -67,9 +83,36 @@ public class LazyCamera :KtisisWindow {
 				Ktisis.Log.Debug("LazyCamera: Removing camera: " + cam.Name);
 				this._ctx.Cameras.RemoveCamera(cam);
 			}
+
+			if(cam == this._ctx.Cameras.Current)
+				ImGui.EndDisabled();
+
 			i++;
 		}
 	}
 
-	
+	private unsafe void GetCamData() {
+		EditorCamera? ec = this._ctx.Cameras.Current;
+		if(ec == null) return;
+		ImGui.Text("FoV:" + ec.Camera->GameCamera.FoV.ToString());
+		ImGui.Text("Min FoV:" + ec.Camera->GameCamera.MinFoV.ToString());
+		ImGui.Text("Max FoV:" + ec.Camera->GameCamera.MaxFoV.ToString());
+		ImGui.Text("Far plane:" + ec.Camera->RenderEx->RenderCamera.FarPlane.ToString());
+		ImGui.Text("Near plane:" + ec.Camera->RenderEx->RenderCamera.NearPlane.ToString());
+		ImGui.Text("Finite near plane:" + ec.Camera->RenderEx->RenderCamera.FiniteFarPlane.ToString());
+	}
+
+	private unsafe void SetCamFov() {
+		EditorCamera? ec = this._ctx.Cameras.Current;
+		if(ec == null) return;
+		// TODO !!! this isn't restored upon exiting gpose
+		var minfov = 0.69f;
+		if(this._DelimitMinFoV)
+			minfov = 0.21f;
+
+		ec.Camera->GameCamera.MinFoV = minfov;
+		ImGui.SliderFloat("FOV", ref ec.Camera->GameCamera.FoV, minfov, 0.78f, "", ImGuiSliderFlags.AlwaysClamp);
+
+		ImGui.Checkbox("Allow lower FOV", ref this._DelimitMinFoV);
+	}
 }
